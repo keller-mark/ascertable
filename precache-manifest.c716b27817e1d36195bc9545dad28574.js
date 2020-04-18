a915dad4365186bf8071634f1a99851361a9f277
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2020-04-18 18:29:46 -0400

    Updates

---
+++ precache-manifest.c716b27817e1d36195bc9545dad28574.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7539882aabcfe6fb554f6b3e1b97a43c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55d7c1d9a0a7e4a33f05",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b1898b3427cb7717f68d",
+    "url": "/static/js/2.6726b1c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6726b1c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55d7c1d9a0a7e4a33f05",
+    "url": "/static/js/main.ec6e4e57.chunk.js"
+  },
+  {
+    "revision": "367d085c3106246fa250",
+    "url": "/static/js/runtime-main.777c0f79.js"
+  }
+]);
